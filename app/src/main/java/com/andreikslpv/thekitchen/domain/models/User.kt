39a15c72be2a426b9fec7cf39363cbd4a6de8f99
andreikslpv <andrei.kslpv@yandex.ru>
2023-05-08 09:16:10 +0300

    18: реализован экран Аутентификации

--- app/src/main/java/com/andreikslpv/thekitchen/domain/models/User.kt
+++ app/src/main/java/com/andreikslpv/thekitchen/domain/models/User.kt
@@ -2,11 +2,8 @@ package com.andreikslpv.thekitchen.domain.models
 
 data class User(
     var uid: String = "",
-//    var displayName: String? = "",
-//    var email: String? = "",
-//    var photoUrl: String? = "",
-//    var createdAt: Long = 0L,
     val favorites: ArrayList<String> = arrayListOf(),
     val history: ArrayList<String> = arrayListOf(),
     val defaultExclude: ArrayList<String> = arrayListOf(),
+    val shoppingList: ArrayList<Ingredient> = arrayListOf(),
 )
